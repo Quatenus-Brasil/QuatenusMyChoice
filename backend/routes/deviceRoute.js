127b3f952695a10625a77f1febc88be12bc5fb8c
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2026-08-07 10:14:57 -0300

    esqueci de validar a schema do dispositivo

--- backend/routes/deviceRoute.js
+++ backend/routes/deviceRoute.js
@@ -2,13 +2,15 @@ import express from "express";
 import { createDevice, findAllDevices, findDeviceById, deleteDevice, editDevice } from "../controllers/deviceController.js";
 import { authenticateUser, isAdmin, isManager, authorizedSectors } from "../middlewares/authMiddleware.js";
 import { validId } from "../middlewares/globalMiddleware.js";
+import { validate } from "../middlewares/validationMiddleware.js";
+import { createDeviceSchema, editDeviceSchema } from "../schemas/deviceSchema.js";
 
 const router = express.Router();
 
-router.post("/", authenticateUser, isManager, authorizedSectors("Suporte & Operações"), createDevice);
+router.post("/", authenticateUser, isManager, authorizedSectors("Suporte & Operações"), validate(createDeviceSchema), createDevice);
 router.get("/", authenticateUser, findAllDevices);
 router.get("/:id", authenticateUser, validId, findDeviceById);
 router.delete("/:id", authenticateUser, validId, isManager, authorizedSectors("Suporte & Operações"), deleteDevice);
-router.patch("/:id", authenticateUser, validId, isManager, authorizedSectors("Suporte & Operações"), editDevice);
+router.patch("/:id", authenticateUser, validId, isManager, authorizedSectors("Suporte & Operações"), validate(editDeviceSchema), editDevice);
 
 export default router;
