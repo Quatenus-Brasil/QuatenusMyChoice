046f1f06c54ec0d85615b60c0f58526d7142fa57
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2026-08-18 14:12:34 -0300

    removendo tudo da gama

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import userRoute from "./userRoute.js";
 import itemRoute from "./itemRoute.js";
-import gamaRoute from "./gamaRoute.js";
 import chipRoute from "./chipRoute.js";
 import deviceRoute from "./deviceRoute.js";
 import accessoryRoute from "./accessoryRoute.js";
@@ -13,7 +12,6 @@ const router = Router();
 router.use("/user", userRoute);
 router.use("/upload", uploadRoute);
 router.use("/item", itemRoute);
-router.use("/gama", gamaRoute);
 router.use("/chip", chipRoute);
 router.use("/device", deviceRoute);
 router.use("/accessory", accessoryRoute);
